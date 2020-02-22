7e8b1d9b24f27b45c792955922f7e5ef8a35a33b
Mamadou Bah <mbah@mbah-a01.vmware.com>
2020-02-22 11:55:28 -0500

    correct typo error

--- kubectl-intermediate/assets/kubectx.sh
+++ kubectl-intermediate/assets/kubectx.sh
@@ -2,7 +2,7 @@
 
 cd ~/
 
-echo "You on you $PWD directory"
+echo "You are on the $PWD directory"
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
